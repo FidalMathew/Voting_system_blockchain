78f01664e4f53bbb755cc9047d1ff29adf7f8ce2
Fidal <fidal15perfect@gmail.com>
2022-05-27 13:19:43 +0530

    changed network

--- client-votingSystem/src/App.jsx
+++ client-votingSystem/src/App.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
 import Admin from './Admin'
 import Voter from './Voter'
 import { useContext } from "react";
 import { VoterContext } from "./Context/Context";
 import Home from './Home';
+
+//Routing is present at RouteApp.jsx
+
 function App() {
 
     const { isManager, errorPage } = useContext(VoterContext)
 
-    return (
-        <>
-            {
-                !errorPage ? (isManager ? <Admin /> : <Voter />) : <Home />
-
-            }    </>
-    )
-
+    return (<>{!errorPage ? (isManager ? <Admin /> : <Voter />) : <Home />}</>)
 }
 
 export default App
